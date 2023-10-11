b6bc829d38537f0f2d8da480b9bf7764a3e16254
Camila <User@DESKTOP-9ENJKRL>
2023-10-11 11:32:48 -0300

    add: editar e excluir

--- src/app/views/categorias/excluir-categoria/excluir-categoria.component.ts
+++ src/app/views/categorias/excluir-categoria/excluir-categoria.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CategoriasService } from '../services/categorias.service';
+import { ContatosRoutingModule } from './../../contatos/contatos-routing.module';
+import { Component, OnInit } from '@angular/core';
+import { VisualizarCategoriaViewModel } from '../models/visualizar-categoria.view-models';
 
 @Component({
   selector: 'app-excluir-categoria',
   templateUrl: './excluir-categoria.component.html',
-  styleUrls: ['./excluir-categoria.component.css']
+  styleUrls: ['./excluir-categoria.component.css'],
 })
-export class ExcluirCategoriaComponent {
+export class ExcluirCategoriaComponent implements OnInit {
+  categoriaVM?: VisualizarCategoriaViewModel;
 
+  constructor(
+    private categoriasService: CategoriasService,
+    private toastrService: ToastrService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaVM = this.route.snapshot.data['categoria'];
+  }
+
+  gravar() {
+    this.categoriasService.excluir(this.categoriaVM!.id).subscribe(() => {
+      this.toastrService.success(
+        `A categoria foi excluída com sucesso!`,
+        'Sucesso'
+      );
+
+      this.router.navigate(['/categorias', 'listar']);
+    });
+  }
 }
